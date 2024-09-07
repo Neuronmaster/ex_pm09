74bb1f1a922cb555bb143814684d413102fbd139
neuromancer <neuromancer.ico@gmail.com>
2024-09-07 20:57:23 +0700

    add comp Events

---
+++ horse-club-fronten/src/components/Events.js
@@ -0,0 +1,17 @@
+// src/components/Events.js
+import React from 'react';
+
+const Events = () => {
+  return (
+    <div className="events">
+      <h2>Предстоящие мероприятия</h2>
+      <ul>
+        <li>Мероприятие 1 - Дата</li>
+        <li>Мероприятие 2 - Дата</li>
+        <li>Мероприятие 3 - Дата</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Events;
