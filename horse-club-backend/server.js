e6890b6557bcad6511436802104462d155284289
neuromancer <neuromancer.ico@gmail.com>
2024-09-08 02:10:44 +0700

    delete comment

--- horse-club-backend/server.js
+++ horse-club-backend/server.js
@@ -6,13 +6,12 @@ const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const User = require('./models/User'); // Убедитесь, что путь к модели правильный
-
+const User = require('./models/User');
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const JWT_SECRET = 'your_jwt_secret'; // Используйте более сложный секрет для продакшн среды
+const JWT_SECRET = 'your_jwt_secret'; // TODO: Используйте более сложный секрет для продакшн среды
 const PORT = process.env.PORT || 5001; // Изменен порт на 5001
 
 // Middleware для аутентификации
@@ -29,7 +28,7 @@ const authMiddleware = (req, res, next) => {
   }
 };
 
-// Пример маршрута для регистрации
+// маршрут для регистрации
 app.post('/api/register', async (req, res) => {
   const { name, email, password } = req.body;
 
@@ -43,7 +42,7 @@ app.post('/api/register', async (req, res) => {
   }
 });
 
-// Пример маршрута для авторизации
+//защищенный маршрут для авторизации
 app.post('/api/login', async (req, res) => {
   const { email, password } = req.body;
 
@@ -65,22 +64,22 @@ app.post('/api/login', async (req, res) => {
   }
 });
 
-// Пример защищенного маршрута для получения данных пользователя
+// защищенный маршрута для получения данных пользователя
 app.get('/api/user', authMiddleware, async (req, res) => {
   try {
     const user = await User.findById(req.user.id);
     if (!user) {
       return res.status(404).send({ error: 'Пользователь не найден' });
     }
-    // Пример получения записей пользователя (добавьте реальную логику получения данных)
-    const records = []; // Замените на реальное получение записей
+    // Пример получения записей пользователя
+    const records = []; // TODO: Замените на реальное получение записей
     res.send({ user, records });
   } catch (error) {
     res.status(500).send({ error: 'Ошибка при получении данных пользователя' });
   }
 });
 
-// Подключение к MongoDB без устаревших опций
+// Подключение к MongoDB
 mongoose.connect('mongodb://127.0.0.1:27017/horse-club')
   .then(() => {
     console.log('Подключено к базе данных MongoDB');
